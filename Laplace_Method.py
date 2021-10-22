c36bf1af3b9d17e6f5fc10d6f300574a7fc1fc7e
Homingdung <1204592866@qq.com>
2021-10-22 21:22:49 +0100

    laplace method loop for x

--- Laplace_Method.py
+++ Laplace_Method.py
@@ -1,6 +1,7 @@
 # This is a verification of laplace method for asymptotic analysis
 import scipy.integrate as integrate
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 # define a function f(t)
@@ -9,8 +10,12 @@ def func(t):
     return f
 
 
-# set value for I(x)
+# set value for I(x), as x becomes larger and larger...
 
-x = 10
-result = integrate.quad(lambda t: np.exp(x * func(t)), 7 / 4, np.inf)
-print(result)
+x = [10, 100]
+y = []
+for i in range(len(x)):
+    result = integrate.quad(lambda t: np.exp(x[i] * func(t)), 7 / 4, np.inf)
+    y.append(result[0])
+
+print(y)
